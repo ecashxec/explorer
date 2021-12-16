3029de7408fa120baa27dab31799f6695a08173d
Adrian Dinis <adriandinis99@gmail.com>
2021-12-16 18:51:17 +0000

    Refactor pagination & state functions to common.js

--- explorer-server/code/common.js
+++ explorer-server/code/common.js
@@ -105,3 +105,194 @@ function minifyHash(hash) {
 }
 
 const generateRange = (start, end) => [...Array(end - start + 1)].map((_, i) => start + i);
+
+const findClosest = (haystack, needle) => (
+  haystack.reduce((a, b) => (
+    Math.abs(b - needle) < Math.abs(a - needle) ? b : a
+  ))
+);
+
+(function(state, $) {
+  const DEFAULT_PAGE = 1;
+  const DEFAULT_ROWS_PER_PAGE = 100;
+  const DEFAULT_ORDER = 'desc';
+
+  const validatePaginationInts = (value, fallback) => {
+    parsedValue = parseInt(value);
+    return isNaN(parsedValue) ? fallback : Math.max(parsedValue, 1);
+  };
+
+  state.getBlockHeight = () => $('#pagination').data('last-block-height');
+
+  state.getParameters = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const page = validatePaginationInts(urlParams.get('page'), DEFAULT_PAGE) - 1;
+    const humanPage = page + 1;
+    const rows = validatePaginationInts(urlParams.get('rows'), DEFAULT_ROWS_PER_PAGE);
+    const order = urlParams.get('order') || DEFAULT_ORDER;
+    const start = parseInt(urlParams.get('start')) || 0;
+    const end = parseInt(urlParams.get('end')) || state.getBlockHeight();
+
+    return { page, humanPage, rows, order, start, end };
+  }
+
+  state.updateParameters = params => {
+    const path = window.location.pathname;
+    const currentURLParams = Object.fromEntries(new URLSearchParams(window.location.search).entries());
+    const newURLParams = new URLSearchParams({ ...currentURLParams, ...params });
+
+    window.history.pushState('', document.title, `${path}?${newURLParams.toString()}`);
+  }
+
+  state.updateLoading = status => {
+    if (status) {
+      $('.loader__container').removeClass('display-none');
+      $('#pagination').addClass('hidden');
+      $('#footer').addClass('hidden');
+    } else {
+      $('.loader__container').addClass('display-none');
+      $('#pagination').removeClass('hidden');
+      $('#footer').removeClass('hidden');
+    }
+  };
+}(window.state = window.state || {}, jQuery));
+
+
+(function(pagination, $) {
+  const activeItem = '<a class="item active" href="HREF" onclick="goToPage(event, NUMBER)">NUMBER</a>';
+  const disabledItem = '<div class="disabled item">...</div>';
+  const item = '<a class="item" href="HREF" onclick="goToPage(event, NUMBER)">NUMBER</a>';
+
+  const determinePaginationSlots = lastPage => {
+    let availableWidth = $('.ui.container').width();
+
+    // pagination slot
+    const padding = 2 * 16;
+    const letter = 8;
+    const tier1 = padding + 1 * letter;
+    const tier2 = padding + 2 * letter;
+    const tier3 = padding + 3 * letter;
+    const tier4 = padding + 4 * letter;
+
+    let predictedTier;
+    if (lastPage > 0 && lastPage < 9) {
+      predictedTier = tier1;
+    } else if (lastPage > 9 && lastPage <= 99) {
+      predictedTier = tier2;
+    } else if (lastPage > 99 && lastPage <= 999) {
+      predictedTier = tier3;
+    } else if (lastPage > 999 && lastPage <= 9999) {
+      predictedTier = tier4;
+    }
+
+    availableWidth -= tier1
+    availableWidth -= predictedTier
+
+    return Math.round((availableWidth) / predictedTier);
+  };
+
+  pagination.generatePaginationRequestParams = () => {
+    const { page, rows, start, end } = window.state.getParameters();
+
+    const startPosition = end - (page * rows);
+    const endPosition = Math.max(startPosition - rows, start);
+
+    return [ startPosition, endPosition ];
+  };
+
+  const generatePaginationArray = (currentPage, max, slots) => {
+    let increments;
+
+    if (slots <= 6) {
+      increments = [1, 100, 500, 1000, 2000, 4000];
+    }
+    else if (slots <= 10) {
+      increments = [1, 10, 50, 100, 500, 1000, 2000, 4000];
+    }
+    else {
+      increments = [1, 2, 10, 20, 50, 100, 500, 1000, 2000, 4000];
+    }
+
+    let pageArray = [];
+
+    for (i = 0; i < Math.floor(slots / 2); i++) {
+      const currentIncrement = increments[i];
+
+      if (!currentIncrement || (currentPage - currentIncrement <= 1)) {
+        break;
+      }
+
+      const value = currentPage - currentIncrement
+      const precision = String(value).length - 1
+
+      if (currentIncrement >= 10) {
+        pageArray.push(parseFloat(value.toPrecision(precision)));
+      } else {
+        pageArray.push(value);
+      }
+    }
+
+    pageArray = pageArray.reverse();
+    if (currentPage != 1) { pageArray.push(currentPage) };
+
+    const remainingSlots = slots - pageArray.length;
+    for (i = 0; i < remainingSlots; i++) {
+      const currentIncrement = increments[i];
+      const value  = currentPage + currentIncrement;
+
+      if (!currentIncrement || (value > max)) {
+        break;
+      }
+
+      const precision = String(value).length - 1
+
+      if (currentIncrement >= 10) {
+        pageArray.push(parseFloat(value.toPrecision(precision)));
+      } else {
+        pageArray.push(value);
+      }
+    }
+
+    if (currentPage == max) { pageArray.pop() };
+    return pageArray;
+  };
+
+  pagination.generatePaginationUIParams = () => {
+    const { humanPage: currentPage, rows } = window.state.getParameters();
+    const blockHeight = getBlockHeight();
+    const lastPage = Math.ceil(blockHeight / rows);
+
+    const slots = determinePaginationSlots(lastPage);
+
+    const pageArray = generatePaginationArray(currentPage, lastPage, slots)
+    pageArray.unshift(1)
+    pageArray.push(lastPage)
+
+    return { currentPage, pageArray };
+  };
+
+  pagination.generatePaginationUI = (currentPage, pageArray) => {
+    const path = window.location.pathname;
+
+    // DOM building blocks
+    const activeItem = (number) => `<a class="item active">${number}</a>`;
+    const item = (number) => `<a class="item" href="${path}?page=${number}" onclick="goToPage(event, ${number})">${number}</a>`;
+
+    let pagination = '';
+    pagination += '<div class="ui pagination menu">';
+
+    pageArray.forEach((pageNumber, i) => {
+      if (pageNumber === currentPage) {
+        pagination += activeItem(pageNumber)
+        return;
+      }
+
+      pagination += item(pageNumber);
+    });
+
+    pagination += '</div>';
+
+    $('#pagination').html(pagination);
+  };
+
+}(window.pagination = window.pagination || {}, jQuery));
